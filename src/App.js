646bbe698ec93a0ba82ff6c44ef34ac715461f78
udit <uditeeiot@gmailcom>
2022-08-07 04:21:36 +0530

    Localstorage and useffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect} from 'react';
 const api = {
-  key: "117ee5b7e1ead1528996c0d54c1b9859",
+  key: "598e497f8dc245c60954fc0fd8b96cfe",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
-  const [query, setQuery] = useState("Mumbai");
+  const [query, setQuery] = useState(localStorage.store);
   const [weather, setWeather] = useState({});
 
-  const search = evt => {
-    if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
-          setWeather(result);
-          setQuery('');
-          console.log(result);
-        });
-    }
-  }
+  // const search = evt => {
+  //   if (evt.key === "Enter") {
+  //     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+  //       .then((res) => 
+  //         console.log(res)
+  //         )
+  //       .then(result => {
+  //         setWeather(result);
+  //         setQuery('');
+  //         console.log(result);
+  //       });
+  //   }
+  // }
 
+  // using use effect
+  useEffect (
+    ()=>{
+        const search =async () =>
+        {
+            const fetch_f= await fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`);
+            const res=await fetch_f.json();
+            setWeather(res);
+            console.log(res);
+        };
+        search();
+    },[query]
+  );
+  const restore=()=>
+  {
+      let a=localStorage.getItem("store");
+      console.log(a);
+      setQuery(a);
+  }
+  const store =(e)=>
+  {
+   if(e.key==="Enter")
+   {
+    localStorage.setItem("store",query);
+   }
+  }
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -33,16 +61,16 @@ function App() {
   }
 
   return (
-    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
-      <main>
-        <div className="search-box">
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 30) ? 'app warm' : 'app') : 'app'}>
+      <main onLoad={restore}>
+        <div className="search-box" >
           <input 
             type="text"
             className="search-bar"
             placeholder="Search..."
             onChange={e => setQuery(e.target.value)}
             value={query}
-            onKeyPress={search}
+            onKeyPress={store}
           />
         </div>
         {(typeof weather.main != "undefined") ? (
@@ -58,7 +86,18 @@ function App() {
             <div className="weather">{weather.weather[0].main}</div>
           </div>
         </div>
-        ) : ('')}
+        ) : (<div>
+          <div className="location-box">
+            <div className="location"></div>
+            <div className="date"></div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+             Search For City
+            </div>
+            <div className="weather"></div>
+          </div>
+        </div>)}
       </main>
     </div>
   );
